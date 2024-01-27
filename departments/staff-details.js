f6fc3077c4bc1e4d9c2d4ef7c03792dcf3abd0fa
Dileep-raj <dileeprajb949@gmail.com>
2024-01-27 20:54:29 +0530

    Modified gallery and departments and added bootstrap 5

--- departments/staff-details.js
+++ departments/staff-details.js
@@ -6,21 +6,25 @@ function loadStaffDetails(staffsJSON) {
         const image = profileDetails["profile-img"], education = profileDetails["education"], subject = profileDetails["subject"]
         const date = profileDetails["date-of-joining"], experience = profileDetails["experience"], extra = profileDetails["extra"]
         const card = document.createElement("div");
-        card.className = "card staff-card"
+        card.className = "card h-100 staff-card"
         var cardInnerHTML = `
-            <img class="card-img-top staff-profile-img" src="${image == "" ? "blank.webp" : image}" alt="Staff Profile image">
-            <div class="card-body">
-                <h4 class="card-title">${staff}</h4>
-                <p class="card-text">`;
+        <img class="card-img-top staff-profile-img" src="${image == "" ? "../blank.webp" : image}" alt="Staff Profile image">
+        <div class="card-body">
+            <h4 class="card-title">${staff}</h4>
+            <p class="card-text">`;
 
         if (extra != "" && extra != null) cardInnerHTML += `${extra} <br>`
         if (education != "") cardInnerHTML += `Education : ${education} <br>`
         if (date != "") cardInnerHTML += `Date of joining : ${date} <br>`
 
-        if (subject != "") cardInnerHTML += `Subject : ${subject} <br>`
+        cardInnerHTML += `Subject : ${subject} <br>`
         cardInnerHTML += `Teaching Experience : ${experience} years`
         cardInnerHTML += `</p></div>`;
         card.innerHTML = cardInnerHTML
-        staffDetails.appendChild(card)
+
+        const col = document.createElement("div")
+        col.className = "col"
+        col.appendChild(card)
+        staffDetails.appendChild(col)
     }
 }
