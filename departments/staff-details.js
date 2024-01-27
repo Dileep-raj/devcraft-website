f109c2a434c7a10a4e92d1ef295fbc0303d8a912
Dileep-raj <dileeprajb949@gmail.com>
2024-01-27 21:07:41 +0530

    Fixed staff details.js error

--- departments/staff-details.js
+++ departments/staff-details.js
@@ -8,14 +8,14 @@ function loadStaffDetails(staffsJSON) {
         const card = document.createElement("div");
         card.className = "card h-100 staff-card"
         var cardInnerHTML = `
-        <img class="card-img-top staff-profile-img" src="${image == "" ? "../blank.webp" : image}" alt="Staff Profile image">
-        <div class="card-body">
-            <h4 class="card-title">${staff}</h4>
-            <p class="card-text">`;
+            <img class="card-img-top staff-profile-img" src="${image == "" ? "../blank.webp" : image}" alt="Staff Profile image">
+            <div class="card-body">
+                <h4 class="card-title">${staff}</h4>
+                <p class="card-text">`;
 
         if (extra != "" && extra != null) cardInnerHTML += `${extra} <br>`
-        if (education != "") cardInnerHTML += `Education : ${education} <br>`
-        if (date != "") cardInnerHTML += `Date of joining : ${date} <br>`
+        if (education != "" && education != null) cardInnerHTML += `Education : ${education} <br>`
+        if (date != "" && date != null) cardInnerHTML += `Date of joining : ${date} <br>`
 
         if (subject != "" && subject != null) cardInnerHTML += `Subject : ${subject} <br>`
         cardInnerHTML += `Teaching Experience : ${experience} years`
